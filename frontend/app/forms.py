971aad9f32961e2c48e270cd66ed036e508b98f4
Javi <jviix8@gmail.com>
2024-03-18 18:25:14 +0100

    Typo en forms

--- frontend/app/forms.py
+++ frontend/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, BooleanField, FileField, EmailField)
+from wtforms import (StringField, PasswordField, BooleanField, FileField, EmailField, SubmitField)
 from wtforms.validators import InputRequired, Length, Email, DataRequired
 
 class LoginForm(FlaskForm):
